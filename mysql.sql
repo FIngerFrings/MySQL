ef66abc7c30814dae4d80df0d332d26ffee5d2ba
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-04-11 23:43:43 +0800

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -12,3 +12,13 @@ select e.deptno, avg(s.grade) as 'sal<grade>' from emp as e join salgrade as s o
  select sal from emp order by sal desc limit 1;
  /* 第二种方案 子连接，找出a表中所有小于b表中sal的值，然后不在里面的就是最大值*/
  select sal from emp where sal not in (select distinct a.sal from emp a join emp b on a.sal < b.sal);
+ 
+ /* 5.取得平均薪水最高的部门的部门编号（至少给出两种解决方案）*/
+ /* 第一种方案 降序取第一个*/
+ select deptno from (select deptno, avg(sal) as avgsal from emp group by deptno) as a order by avgsal desc limit 1
+ /* 第二种方案 用max找出最大值*/
+ select a.deptno, max(a.avgsal) from (select deptno, avg(sal) as avgsal from emp group by deptno) as a;
+ /* 第二种方案会显示出部门编号和平均薪资，下面这一句实际上就是在上面这一句又套了一层，但能只显示出部门编号*/
+ select b.deptno from (select a.deptno, max(a.avgsal) from (select deptno, avg(sal) as avgsal from emp group by deptno) as a) as b;
+ /* 第三种方案 先找出最大值，然后使用where语句部门编号*/
+ select a.deptno from (select deptno, avg(sal) as avgsal from emp group by deptno) as a where a.avgsal = (select max(a.avgsal) from (select deptno, avg(sal) as avgsal from emp group by deptno) as a);
