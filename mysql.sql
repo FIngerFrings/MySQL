689b519662652945c0d372d1ddfcd0f4a6ecaa43
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-04-11 22:06:38 +0800

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -8,4 +8,7 @@ select e.ename, e.sal from emp as e join (select avg(sal) as avgsal, deptno from
 select e.deptno, avg(s.grade) as 'sal<grade>' from emp as e join salgrade as s on e.sal between losal and hisal group by deptno;
 
 /* 4.不准用组函数（Max），取得最高薪水（给出两种解决方案）*/
-
+/* 第一种方案 降序取第一个*/
+ select sal from emp order by sal desc limit 1;
+ /* 第二种方案 子连接，找出a表中所有小于b表中sal的值，然后不在里面的就是最大值*/
+ select sal from emp where sal not in (select distinct a.sal from emp a join emp b on a.sal < b.sal);
